40d70f34e753f72a334d70adf2ab61e7aae9bad0
munkhsukhjigjid <munkhsukh.jigjid@gmail.com>
2024-12-09 15:53:52 +0800

    codegen

--- src/configs/HOST_CONFIG.ts
+++ src/configs/HOST_CONFIG.ts
@@ -1,7 +1,7 @@
 export const HOST_CONFIG = {
-  host: 'https://api.matsup.mn',
+  host: 'http://154.38.182.159:3001',
   elastic: {
-    host: 'https://es.matsup.mn',
+    host: 'https://154.38.182.159:9200',
     username: 'elastic',
     password: 'c0ec29451bcce11a',
   },
