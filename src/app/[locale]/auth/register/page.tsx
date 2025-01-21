b2fcb7ac4fee86c68fcf433d45afd022bd0b86cb
Mason-bldrj <baterdene.bldrj@gmail.com>
2025-01-21 16:05:06 +0800

    zasvar

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -1,17 +1,11 @@
 'use client';
 
+import AuthRegisterClient from '@/components/page-client/auth/auth-register-client';
+
 export default function AuthRegister() {
   return (
     <div className="container flex items-center justify-center py-10">
       <AuthRegisterClient />
     </div>
   );
-}
-
-interface RegisterFormValues {
-  login: string;
-  token: string;
-  firstName: string;
-  lastName: string;
-  password: string;
 }
