b4f507342739ce1eca3e85503953c844bc976997
Mason-bldrj <baterdene.bldrj@gmail.com>
2025-01-23 11:51:49 +0800

    zasvar

--- src/components/user-dashboard/side-bar.tsx
+++ src/components/user-dashboard/side-bar.tsx
@@ -4,14 +4,13 @@ import { Eye, Heart, IdCard, ListOrdered, User } from 'lucide-react';
 import Link from 'next/link';
 
 export function SideBar() {
-  return(
+  return (
     <div className=" ml-4  flex h-full w-[20%] flex-col gap-4 px-2 py-2 shadow-lg">
-    {SideBarItems.map((element, index) => (
-      <SideBarItem key={index} link={element.link} icon={element.icon} text={element.name} />
-    ))}
-  </div>
-  )
-  
+      {SideBarItems.map((element, index) => (
+        <SideBarItem key={index} link={element.link} icon={element.icon} text={element.name} />
+      ))}
+    </div>
+  );
 }
 
 const SideBarItem = (props: CardDataType) => (
