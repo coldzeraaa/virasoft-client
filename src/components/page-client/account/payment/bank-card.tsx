6c2e04f6cf5fadfe3023afdbf679218687e92a52
Mason-bldrj <baterdene.bldrj@gmail.com>
2025-01-24 09:18:37 +0800

    modal hiiv

--- src/components/page-client/account/payment/bank-card.tsx
+++ src/components/page-client/account/payment/bank-card.tsx
@@ -1,26 +1,56 @@
 'use client';
 
+import { useRef } from 'react';
+
 import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-export function PaymentCard({ bankList }: { bankList: BankListType }) {
+export function PaymentCard({ bankList, deviceType }: { bankList: BankListType; deviceType: string | undefined }) {
+  const router = useRouter();
+  const modalRef = useRef<HTMLDialogElement>(null);
   return (
-    <div className="flex  w-[300px] items-center justify-between  gap-3  rounded-xl border-[1px] border-solid border-gray-200 px-2 py-2 hover:shadow-lg">
+    <div className="flex  w-full items-center justify-between  gap-3  rounded-xl border-[1px] border-solid border-gray-200 px-2 py-2 hover:shadow-lg">
       <div className="flex items-center gap-2">
         <div className="flex h-10 w-10 items-center justify-center">
           <Image priority width={800} height={800} src={`${bankList.logo}`} alt="" />
         </div>
-        <div>
-          {bankList?.name ?? <p className=" text-ls font-medium">{bankList?.name}</p>}
-          {bankList?.description ?? <p className="text-sm text-gray-400">{bankList?.description}</p>}
+        <div className="flex flex-col">
+          <div>{bankList?.name ?? <p className=" text-ls font-medium">{bankList?.name}</p>}</div>
+          <div>{bankList?.description ?? <p className="text-sm text-gray-400">{bankList?.description}</p>}</div>
         </div>
       </div>
       <div>
-        <Link href={`${bankList?.link}`}>
+        <button
+          type="button"
+          onClick={() => {
+            if (deviceType === 'desktop') {
+              modalRef.current?.showModal();
+            } else if (deviceType === 'mobile') {
+              router.push(bankList.link);
+            }
+          }}
+        >
           <ArrowRight />
-        </Link>
+        </button>
       </div>
+      <dialog ref={modalRef} className="modal">
+        <div className="modal-box w-fit  max-w-5xl">
+          <div className="flex items-center justify-center">
+            <Image
+              width={400}
+              height={200}
+              src="https://upload.wikimedia.org/wikipedia/commons/d/d0/QR_code_for_mobile_English_Wikipedia.svg"
+              alt="image"
+            />
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 }
--- src/components/page-client/account/payment/payment-page.tsx
+++ src/components/page-client/account/payment/payment-page.tsx
@@ -1,26 +1,38 @@
 'use client';
+import { useEffect, useState } from 'react';
+
 import { BankListType, PaymentCard } from './bank-card';
 
+import { ErrorResult } from '@/components/result/error-result';
+import { LoadingResult } from '@/components/result/loading-result';
 import { useMyOrderQuery } from '@/gql/query/user/my-order.generated';
 
 export default function PaymentPage({ params }: { params: { number: string } }) {
-  const { data } = useMyOrderQuery({
+  const { data, loading, error } = useMyOrderQuery({
     variables: { number: params.number },
   });
+
+  const [deviceType, setDeviceType] = useState<string>();
+  useEffect(() => {
+    const isDesktop = window.matchMedia('(min-width: 1024px) and (hover: hover)').matches;
+    setDeviceType(isDesktop ? 'desktop' : 'mobile');
+  }, []);
+  if (loading) return <LoadingResult />;
+  if (error) return <ErrorResult message={error.message} />;
 
   return (
     <div>
       <h2 className="text-2xl">Төлбөрийн төрөл</h2>
-      <div className="flex flex-wrap gap-6 py-3">
-        {data?.myOrder?.payments[0].source?.bank_list?.map((element: BankListType, idx: number) => (
-          <PaymentCard key={idx} bankList={element} />
+      <div className="py-w flex flex-wrap gap-3">
+        {data?.myOrder?.payments[0]?.source?.bank_list?.map((element: BankListType, idx: number) => (
+          <PaymentCard key={idx} deviceType={deviceType} bankList={element} />
         ))}
       </div>
       <hr />
       <h2 className="text-2xl">Хувааж төлөх нөхцөл</h2>
       <div>
         <div className="flex w-fit items-center justify-center gap-3 rounded-xl border-[1px] border-solid border-gray-200 px-2 py-2 hover:shadow-lg">
-          Тун удахгүй
+          {deviceType === 'desktop' ? 'Desktop Device' : 'Mobile Device'}
         </div>
       </div>
     </div>
