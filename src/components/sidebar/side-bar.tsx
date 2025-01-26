bc0b2d737105e6cd0e3f87e35bd3cc07b7a15f0b
Mason-bldrj <baterdene.bldrj@gmail.com>
2025-01-26 17:17:02 +0800

    Logout буюу  гарах товч хийв

--- src/components/sidebar/side-bar.tsx
+++ src/components/sidebar/side-bar.tsx
@@ -1,14 +1,33 @@
+'use client';
 import { ReactElement } from 'react';
 
-import { IdCard, ListOrdered } from 'lucide-react';
+import cookies from 'js-cookie';
+import { IdCard, ListOrdered, User } from 'lucide-react';
 import Link from 'next/link';
+
+import { STORE_KEY_CONFIG } from '@/configs/STORE_KEY_CONFIG';
 
 export function SideBar() {
   return (
-    <div className=" ml-4  flex h-full w-[20%] flex-col gap-4 px-2 py-2 shadow-lg">
-      {sidebarItems.map((element, index) => (
-        <SideBarItem key={index} link={element.link} icon={element.icon} text={element.name} />
-      ))}
+    <div className="justify-betwee ml-4 flex  h-full  w-[20%] flex-col shadow-lg">
+      <div className="   flex h-full  flex-col gap-4 px-2 py-2 ">
+        {sidebarItems.map((element, index) => (
+          <SideBarItem key={index} link={element.link} icon={element.icon} text={element.name} />
+        ))}
+      </div>
+      <div className="flex flex-col items-center justify-center gap-3">
+        <User className="h-8 w-8" />
+        <button
+          onClick={() => {
+            cookies.remove(STORE_KEY_CONFIG.NEXT_USER_TOKEN);
+            window.location.href = '/auth/login';
+          }}
+          type="button"
+          className="btn w-[70%] "
+        >
+          Гарах
+        </button>
+      </div>
     </div>
   );
 }
