a61a4d1a67263e9acc89d64207bc02fb2e6c6741
Mason-bldrj <baterdene.bldrj@gmail.com>
2025-01-30 13:36:49 +0800

    zasvar

--- src/components/page-client/account/orders/[number]/order-detail-section.tsx
+++ src/components/page-client/account/orders/[number]/order-detail-section.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
+
 import { MyOrderType } from './order-info';
 
 import { moneyFormatHelper } from '@/lib/helper/format/money-format-helper';
+import { imageUrlHelper } from '@/lib/helper/img-url-helper';
 
 export function OrderDetailSection({ myOrder }: MyOrderType) {
   return (
@@ -22,7 +25,9 @@ export function OrderDetailSection({ myOrder }: MyOrderType) {
               <p className="text-base font-medium text-gray-700 sm:text-lg">Барааны нэр: {element.variant.product.name}</p>
               <p className="text-sm text-gray-500 sm:text-base">Үнэ: {moneyFormatHelper(element.variant.price)}₮</p>
             </div>
-
+            <div className="flex h-[60px] w-[60px] items-center justify-center rounded-md border border-solid border-gray-400 p-1">
+              <Image width={500} height={500} src={imageUrlHelper(element?.variant?.images[0])} alt="" />
+            </div>
             {/* Quantity and Total */}
             <div className="mt-2 flex w-full flex-col items-end sm:mt-0 sm:w-1/3">
               <p className="text-base font-semibold text-gray-900 sm:text-lg">
