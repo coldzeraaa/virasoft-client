663365dfaaf09ee823ca4f6344c74b9d45ae9c62
Haliukaaa <haliuka.aqua@gmail.com>
2025-02-05 16:45:47 +0800

    added price range slider

--- src/app/[locale]/account/layout.tsx
+++ src/app/[locale]/account/layout.tsx
@@ -5,9 +5,9 @@ import { SideBar } from '@/components/sidebar/side-bar';
 export default function DashboardLayout({ children }: ChildrenType) {
   return (
     <div className="h-[calc(100%-64px)] w-full bg-base-200">
-      <div className="container grid h-full max-w-7xl grid-cols-12 py-6">
+      <div className="container grid h-full max-w-7xl grid-cols-12 gap-5 py-6">
         <SideBar />
-        <div className="col-span-12 h-full w-full px-8 md:col-span-8 lg:col-span-9">{children}</div>
+        <div className="col-span-12 h-full w-full md:col-span-8 lg:col-span-9">{children}</div>
       </div>
     </div>
   );
--- src/components/algolia/filters.tsx
+++ src/components/algolia/filters.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
-import { useRefinementList } from 'react-instantsearch';
+import { useRange, useRefinementList } from 'react-instantsearch';
 
 const OptionFilter = ({ attribute, title, optionKey }: { attribute: string; title: string; optionKey: string }) => {
   const { items, refine } = useRefinementList({ attribute });
@@ -39,7 +39,14 @@ const OptionFilter = ({ attribute, title, optionKey }: { attribute: string; titl
 
 export const Filters = () => {
   const { items } = useRefinementList({ attribute: 'options' });
+  const { range, refine } = useRange({ attribute: 'price' });
+  const { min, max } = range;
 
+  const [price, setPrice] = useState(min);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(Number(e.target.value));
+    refine([Number(e.target.value), max]);
+  };
   const optionTypes = useMemo(() => {
     const uniqueKeys = new Set(items.map((item) => item.label.split('||')[0]));
 
@@ -60,6 +67,19 @@ export const Filters = () => {
         {optionTypes.map(({ key, title }) => (
           <OptionFilter key={key} attribute="options" title={title} optionKey={key} />
         ))}
+      </div>
+      <label className="mb-2 text-sm font-medium">Үнэ: {price}₮</label>
+      <input
+        type="range"
+        className="range range-primary range-xs w-full"
+        min={min}
+        max={max}
+        value={price}
+        onChange={(e) => handleChange(e)}
+      />
+      <div className="mt-2 flex w-full justify-between text-xs">
+        <span>{min}₮</span>
+        <span>{max}₮</span>
       </div>
     </div>
   );
