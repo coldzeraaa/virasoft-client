b764341a61eccdc915ac275564dd7e6095d18c1f
Haliukaaa <haliuka.aqua@gmail.com>
2025-02-06 14:32:34 +0800

    bsent header done

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import './globals.css';
 
 import { bodyClassName } from '@/components/class-names/body-class-names';
 import { lightTheme } from '@/components/class-names/data-theme-name';
+import { montserrat } from '@/components/class-names/font-families/index.bsent';
 import { Header } from '@/components/header';
 import { APP_CONFIG } from '@/configs/APP_CONFIG';
 import { AuthProvider } from '@/lib/context/auth-context';
@@ -15,7 +16,7 @@ import { ApolloProvider } from '@/lib/provider/apollo-provider';
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html lang="en" data-theme={lightTheme}>
+    <html lang="en" data-theme={lightTheme} className={`${montserrat.className}`}>
       <body className={bodyClassName}>
         <ApolloProvider>
           <AuthProvider>
---
+++ src/configs/colors.bsent.ts
@@ -0,0 +1,62 @@
+export const LIGHT_COLORS = {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  ...require('daisyui/src/theming/themes')['light'],
+  primary: '#102F31', // emerald green
+  'primary-content': '#FFFFFF',
+  secondary: '#C09049', // golden
+  'secondary-content': '#FFFFFF',
+  accent: '#D88100', // bright orange
+  'accent-content': '#FFFFFF',
+  neutral: '#272727', // muted dark grey
+  'neutral-content': '#FFFFFF',
+  'base-100': '#FBFAEE', // off-white beige
+  'base-200': '#F6F7F9',
+  'base-300': '#E7EAEF',
+  'base-content': '#000C38',
+  info: '#00BFFF',
+  'info-content': '#FFFFFF',
+  success: '#32CD32',
+  'success-content': '#FFFFFF',
+  warning: '#FFA500',
+  'warning-content': '#FFFFFF',
+  error: '#B22222',
+  'error-content': '#FFFFFF',
+};
+
+export const DARK_COLORS = {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  ...require('daisyui/src/theming/themes')['dark'],
+  primary: '#DAA520', // darker golden color
+  'primary-content': '#FDF0EC', // grey with pinkish hue
+  secondary: '#6B8E23', // olive green
+  'secondary-content': '#EDF2F8', // neutral light grey
+  accent: '#FF4500', // orange red
+  'accent-content': '#222529', // black
+  neutral: '#FFFFFF', // dark slate grey
+  'neutral-content': '#6C707D', // muted grey with blue undertone
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  'base-100': require('daisyui/src/theming/themes')['dark']['base-100'],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  'base-200': require('daisyui/src/theming/themes')['dark']['base-200'],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  'base-300': require('daisyui/src/theming/themes')['dark']['base-300'],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  'base-content': require('daisyui/src/theming/themes')['dark']['base-content'],
+  info: '#00BFFF', // deep sky blue
+  'info-content': '#FFFFFF', // white
+  success: '#32CD32', // lime green
+  'success-content': '#FFFFFF', // white
+  warning: '#FFA500', // orange
+  'warning-content': '#FFFFFF', // white
+  error: '#B22222', // firebrick
+  'error-content': '#FFFFFF', // white
+};
+
+const themes = {
+  daisyui: {
+    logs: false,
+    themes: [{ dark: DARK_COLORS, light: LIGHT_COLORS }],
+  },
+};
+
+export default themes;
