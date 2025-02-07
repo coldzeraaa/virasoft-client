73c17fa114ff0a6657fde6150ae926fecb5afcaf
munkhsukhjigjid <munkhsukh.jigjid@gmail.com>
2025-02-07 09:06:00 +0800

    number input is updated

--- src/components/form/inputs/number-input.tsx
+++ src/components/form/inputs/number-input.tsx
@@ -1,50 +1,42 @@
 import { createRef } from 'react';
 
+import type { AnyType } from '@/types/global-type';
+
 export function NumberInput({ length = 4, value, onChange = () => true, disabled }: NumberInputProps) {
   const refs = Array.from({ length }).map(() => createRef<HTMLInputElement>());
   const values = value?.split('') || Array.from({ length }).map(() => '');
 
   return (
-    <ul className={`grid ${spans[length]} gap-2`}>
-      {Array.from({ length }).map((_, index) => (
-        <li key={index}>
-          <input
-            ref={refs[index]}
-            disabled={disabled}
-            value={values[index] || ''}
-            placeholder="*"
-            onKeyDownCapture={(event) => {
-              if (event.key === 'Backspace' && !values[index] && index - 1 >= 0) {
-                refs[index - 1].current?.focus();
-                refs[index - 1].current?.select();
-              }
-              if (event.key === 'ArrowLeft' && index - 1 >= 0) refs[index - 1].current?.focus();
-              if (event.key === 'ArrowRight' && index + 1 < length) refs[index + 1].current?.focus();
-            }}
-            onChange={(event) => {
-              if (event.target.value.length > 1) return onChange(event.target.value.slice(0, length));
-              if (event.target.value) {
-                if (index + 1 < length) refs[index + 1].current?.focus();
-              } else if (index - 1 >= 0) {
-                refs[index - 1].current?.focus();
-                refs[index - 1].current?.select();
-              }
-              if (event.target.value && index + 1 === length) refs[index].current?.select();
-              values[index] = event.target.value;
-              onChange(values.join(''));
-            }}
-            onPaste={(event) => {
-              const paste = event.clipboardData.getData('text');
-              onChange(paste.slice(0, length));
-              refs[paste.length > length ? length - 1 : paste.length].current?.focus();
-            }}
-            onSelect={() => refs[index].current?.select()}
-            className="input input-bordered aspect-square w-full appearance-none text-center"
-            maxLength={1}
-          />
-        </li>
+    <div className={`grid ${spans[length]} gap-2`}>
+      {Array.from({ length }).map((_, idx) => (
+        <input
+          key={idx}
+          ref={refs[idx]}
+          disabled={disabled}
+          value={values[idx] || ''}
+          placeholder="*"
+          onKeyDownCapture={(event) => {
+            if (event.key === 'Backspace' && !values[idx] && idx - 1 >= 0) refs[idx - 1]?.current?.focus();
+            if (event.key === 'ArrowLeft' && idx - 1 >= 0) refs[idx - 1]?.current?.focus();
+            if (event.key !== 'Backspace' && event.key !== 'ArrowLeft' && idx + 1 < length) refs[idx + 1]?.current?.focus();
+          }}
+          onChange={(event) => {
+            if (event.target.value.length > 1) return onChange(event.target.value.slice(0, length));
+            if (idx === length - 1 && event.target.value) (event.target as AnyType).select();
+            values[idx] = event.target.value;
+            onChange(values.join(''));
+          }}
+          onSelect={(event) => (event.target as AnyType).select()}
+          onPaste={(event) => {
+            const paste = event.clipboardData.getData('text');
+            onChange(paste.slice(0, length));
+            refs[paste.length > length ? length - 1 : paste.length]?.current?.focus();
+          }}
+          className="input input-bordered aspect-square w-full appearance-none text-center"
+          maxLength={1}
+        />
       ))}
-    </ul>
+    </div>
   );
 }
 
