22a3281680ca4c12c51aabeb590d2261c2806a67
munkhsukhjigjid <munkhsukh.jigjid@gmail.com>
2025-02-17 10:05:56 +0800

    build-type checkout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -17,49 +17,47 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-40 w-full bg-base-100 shadow-md">
-      <div className="container max-w-7xl">
-        <div className="flex h-16 items-center justify-between">
-          <Link href="/" className="mr-3 flex flex-shrink-0 items-center">
-            <Image className="h-8 w-auto" src={logo} alt="Company Logo" width={32} height={42} priority />
-          </Link>
-          <div className="flex flex-1 items-center justify-end gap-4 md:justify-between">
-            <div className="relative hidden max-w-xs flex-1 md:block">
-              <input
-                type="text"
-                className="w-full rounded-lg border px-4 py-1 text-neutral placeholder-base-content focus:outline-none"
-                placeholder="Бүтээгдэхүүн хайх..."
-                onKeyDown={(event) => {
-                  if (event.key === 'Enter') router.push('/s');
-                }}
-              />
-              <Search className="absolute right-3 top-1.5 h-5 w-5 stroke-1 text-gray-400" />
-            </div>
-
-            <nav className="flex items-center gap-2 text-xs">
-              <ul className="flex">
-                {menuItems(userData).map((item, index) => (
-                  <Link
-                    key={index}
-                    href={item.link}
-                    className={`group relative flex flex-col items-center rounded-lg p-1 text-base-content transition-colors ${
-                      item.title === 'Хадгалсан' || item.title === 'Профайл' ? 'hidden md:flex' : 'md:flex'
-                    } lg:p-2`}
-                  >
-                    {item.title === 'Сагс' && order?.itemCount !== undefined && (
-                      <div className="badge badge-secondary badge-xs absolute right-0 top-0 py-2">
-                        {order?.itemCount}
-                        {orderLoading && <div className="loading loading-ring w-3"></div>}
-                      </div>
-                    )}
-                    {item.icon}
-                    <li className="hidden group-hover:text-secondary md:block">
-                      {item.title === 'Профайл' ? (userData?.me ? userData?.me.firstName : 'Нэвтрэх') : item.title}
-                    </li>
-                  </Link>
-                ))}
-              </ul>
-            </nav>
+      <div className="container flex items-center justify-between">
+        <Link href="/" className="mr-3 flex flex-shrink-0 items-center">
+          <Image className="h-8 w-auto" src={logo} alt="Company Logo" width={32} height={42} priority />
+        </Link>
+        <div className="flex flex-1 items-center justify-end gap-4 md:justify-between">
+          <div className="relative hidden max-w-xs flex-1 md:block">
+            <input
+              type="text"
+              className="w-full rounded-lg border px-4 py-1 text-neutral placeholder-base-content focus:outline-none"
+              placeholder="Бүтээгдэхүүн хайх..."
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') router.push('/s');
+              }}
+            />
+            <Search className="absolute right-3 top-1.5 h-5 w-5 stroke-1 text-gray-400" />
           </div>
+
+          <nav className="flex items-center gap-2 text-xs">
+            <ul className="flex">
+              {menuItems(userData).map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.link}
+                  className={`group relative flex flex-col items-center rounded-lg p-1 text-base-content transition-colors ${
+                    item.title === 'Хадгалсан' || item.title === 'Профайл' ? 'hidden md:flex' : 'md:flex'
+                  } lg:p-2`}
+                >
+                  {item.title === 'Сагс' && order?.itemCount !== undefined && (
+                    <div className="badge badge-secondary badge-xs absolute right-0 top-0 py-2">
+                      {order?.itemCount}
+                      {orderLoading && <div className="loading loading-ring w-3"></div>}
+                    </div>
+                  )}
+                  {item.icon}
+                  <li className="hidden group-hover:text-secondary md:block">
+                    {item.title === 'Профайл' ? (userData?.me ? userData?.me.firstName : 'Нэвтрэх') : item.title}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
--- src/components/pages/page/component/image/index.tsx
+++ src/components/pages/page/component/image/index.tsx
@@ -7,7 +7,15 @@ export function ImageComponent({ images = [], description = '' }: ImageComponent
   const [image] = images;
   if (!image) return null;
 
-  return <Image width={0} height={0} src={imageUrlHelper(image.url)} alt={description} className="h-fit w-full object-contain" />;
+  return (
+    <Image
+      width={image.metadata?.width || 0}
+      height={image.metadata?.height || 0}
+      src={imageUrlHelper(image.url)}
+      alt={description}
+      className="h-fit w-full object-contain"
+    />
+  );
 }
 
 export interface ImageComponentProps {
