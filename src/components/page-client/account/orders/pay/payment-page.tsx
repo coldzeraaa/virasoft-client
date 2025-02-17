ce63e6e9211ae3261c8b97038c980d78263b56d7
munkhsukhjigjid <munkhsukh.jigjid@gmail.com>
2025-02-17 10:59:38 +0800

    build-type checkout

--- src/components/page-client/account/orders/pay/payment-page.tsx
+++ src/components/page-client/account/orders/pay/payment-page.tsx
@@ -1,35 +1,79 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { ReactNode, useState } from 'react';
 
-import { BankListType, PaymentCard } from './bank-card';
+import Image from 'next/image';
 
 import { ErrorResult } from '@/components/result/error-result';
-import { LoadingResult } from '@/components/result/loading-result';
 import { useMyOrderQuery } from '@/gql/query/user/my-order.generated';
 import { generateQCodeHelper } from '@/lib/helper/generate-qrcode-helper';
+import { imageUrlHelper } from '@/lib/helper/img-url-helper';
 
 export default function PaymentPage({ params }: { params: { number: string } }) {
-  const [qrCode, setQrCode] = useState(`https://via.placeholder.com/208?text=QR`);
+  const [qrCode, setQrCode] = useState<string | null>(null);
   const { data, loading, error } = useMyOrderQuery({ variables: { number: params.number } });
   const payments = data?.myOrder?.payments || [];
   const [payment] = payments;
-  // const router = useRouter();
 
-  useEffect(() => {
-    if (payment?.source?.qr_code) generateQCodeHelper(payment.source.qr_code).then(setQrCode);
-  }, [payment?.source?.qr_code]);
+  if (loading)
+    return (
+      <Container>
+        <ul className="flex flex-wrap gap-6">
+          {Array.from({ length: 16 }).map((_, idx) => (
+            <li key={idx} className="min-w-20 flex-1">
+              <div className="skeleton mx-auto mb-2 aspect-square w-20 rounded-lg object-contain" />
+              <div className="skeleton mt-2 h-5 w-full" />
+            </li>
+          ))}
+        </ul>
+      </Container>
+    );
 
-  if (loading) return <LoadingResult />;
-  if (error) return <ErrorResult message={error.message} />;
+  if (error)
+    return (
+      <Container>
+        <ErrorResult message={error.message} />
+      </Container>
+    );
+
+  if (qrCode)
+    return (
+      <Container title="QPay уншуулна уу">
+        <Image className="mx-auto max-w-sm" priority width={800} height={800} src={qrCode} alt="QR code" />
+      </Container>
+    );
 
   return (
-    <>
-      <h2 className="mb-4 text-2xl font-bold text-gray-800">Төлбөрийн төрөл</h2>
-      <div className="flex flex-wrap gap-6">
-        {data?.myOrder?.payments[0]?.source?.bank_list?.map((element: BankListType, idx: number) => (
-          <PaymentCard key={idx} qrCode={qrCode} orderNumber={data.myOrder?.number} bankList={element} />
+    <Container>
+      <ul className="flex flex-wrap gap-6">
+        {data?.myOrder?.payments[0]?.source?.bank_list?.map((element: { logo: string; name?: string }, idx: number) => (
+          <li key={idx} className="min-w-20 flex-1">
+            <button
+              type="button"
+              className="w-full"
+              onClick={() => generateQCodeHelper(payment.source.qr_code).then(imageUrlHelper).then(setQrCode).catch(console.error)}
+            >
+              <Image
+                className="mx-auto mb-2 aspect-square w-20 rounded-lg object-contain"
+                priority
+                width={800}
+                height={800}
+                src={imageUrlHelper(element.logo || '')}
+                alt={element?.name || 'bank name'}
+              />
+              <p className="truncate text-center text-sm font-medium">{element?.name}</p>
+            </button>
+          </li>
         ))}
-      </div>
+      </ul>
+    </Container>
+  );
+}
+
+function Container({ children, title }: { children: ReactNode; title?: string }) {
+  return (
+    <>
+      <h2 className="mb-4 text-center text-2xl font-bold">{title || 'Төлбөр төлөх нөхцөл сонгоно уу'}</h2>
+      {children}
     </>
   );
 }
