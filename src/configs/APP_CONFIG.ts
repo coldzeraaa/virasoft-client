6d6be66666f951d37cba86b7fcfbd93cd8da1851
munkhsukhjigjid <munkhsukh.jigjid@gmail.com>
2025-03-19 11:28:16 +0800

    genrui

--- src/configs/APP_CONFIG.ts
+++ src/configs/APP_CONFIG.ts
@@ -2,13 +2,13 @@ import type { AppConfigType } from './APP_CONFIG_TYPE';
 
 export const APP_CONFIG: AppConfigType = {
   locale: 'mn',
-  locales: ['en'],
-  sourceLocale: 'en',
-  appIndex: 'uc',
+  locales: ['mn'],
+  sourceLocale: 'mn',
+  appIndex: 'Default',
 
   tokens: {
-    uid: 'backoffice',
-    secret: '70194bbc6f42d6f07207252d56fef245a8eb24513f7637eabbda6bdd1d36ba7a',
+    uid: 'Default',
+    secret: 'Default',
   },
   map: {
     defaultCenter: { lat: 47.918, lng: 106.917 },
